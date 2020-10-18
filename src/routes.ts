4e45650ffb9bf3a3537e58c07857dea6d11e5f60
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2020-10-18 13:03:43 -0300

    Updated OrphanagesController

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import uploadConfig from './config/upload';
-import OrphanagesController from './controllers/OrphanagesContoller';
+import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 const upload = multer(uploadConfig);
